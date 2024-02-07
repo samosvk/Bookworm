89437462b077c97139ab706a3e525a6efbc0d46c
toconn23 <toconnn23@nd.edu>
2024-02-07 11:30:18 -0500

    initial commit

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,137 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+
+
+const defaultTheme = createTheme();
+
+export default function LogIn() {
+  const[loginStatus, setLoginStatus] = useState(undefined);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new URLSearchParams(new FormData(event.currentTarget));
+    try {
+      //get the csrfToken
+      const csrfToken = document.cookie
+        .split('; ')
+        .find(row => row.startsWith('csrftoken='))
+        .split('=')[1];
+      const response = await fetch('http://localhost:8000/authenticate/login/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'X-CSRFToken': csrfToken
+      },
+      body: data.toString(),
+      credentials: 'include'
+    });
+      if(response.ok){
+        const data = await response.json();
+        navigate("/");
+        setLoginStatus(true);
+      } else{
+        const errorData = await response.json();
+        console.error('Failed to submit form', errorData);
+        //set the error
+        setLoginStatus(false);
+      }
+    }catch(error){
+        console.error('Error', error);
+        setLoginStatus(false);
+    }
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }} method="post">
+            {/* Include CSRF token in the form */}
+            {/* <input type="hidden" name="csrfmiddlewaretoken" value={csrfToken} /> */}
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Log In
+            </Button>
+            {/* add login feedback */}
+            <div>
+              {loginStatus == false && (
+                <p style={{color: 'red'}}>login failed</p>
+              )}
+            </div>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/register" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
---
+++ frontend/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import index
+
+urlpatterns = [
+    path('', index),
+    path('login/', index),
+    path('register/', index),
+]
