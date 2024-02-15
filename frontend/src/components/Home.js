8c89ed9821da111077955c60bce7cfcb2192491c
toconn23 <toconn23@nd.edu>
2024-02-15 15:42:10 -0500

    add button in top right

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button, ThemeProvider, createTheme } from '@mui/material';
+
+const defaultTheme = createTheme();
+
+const styles = {
+  buttonContainer: {
+    position: 'absolute',
+    top: '20px',
+    right: '20px',
+  },
+};
 
 export default function Homepage() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to My Homepage</h1>
-        <p>This is a blank homepage with a login button.</p>
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
-      </header>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to My Homepage</h1>
+          <p>This is a blank homepage with a login button.</p>
+          <div style={styles.buttonContainer}>
+            <Link to="/login">
+              <Button variant='contained'>Login</Button>
+            </Link>
+          </div>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
+
 
