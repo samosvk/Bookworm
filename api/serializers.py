cbeebf3d45a9def57ea922cf8a63212e3eea1263
toconn23 <toconnn23@nd.edu>
2024-02-27 22:40:42 -0500

    Added FillBlank support

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Book, Text, MultipleChoice, Element
+from .models import Book, Text, MultipleChoice, Element, FillBlank
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
@@ -21,9 +21,16 @@ class TextSerializer(serializers.ModelSerializer):
         model = Text
         fields = ('book', 'text')
 
+class FillBlankSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FillBlank
+        fields = ('book', 'question')
+
 class ElementSerializer(serializers.ModelSerializer):
+    #call get_conent_object to get the content of the element
     content_object = serializers.SerializerMethodField()
 
+    #retrieve content for specific type of element
     def get_content_object(self, obj):
         serializer_class = self.get_serializer_for_instance(obj.content_object)
         if serializer_class is not None:
@@ -31,10 +38,12 @@ class ElementSerializer(serializers.ModelSerializer):
             return serializer.data
         return None
     
+    #assign serializer for the associated type of the elements
     def get_serializer_for_instance(self, instance):
         serializers_map ={
             MultipleChoice: MultipleChoiceSerializer,
-            Text: TextSerializer
+            Text: TextSerializer,
+            FillBlank: FillBlankSerializer
         }
         return serializers_map.get(type(instance), None)
     class Meta:
